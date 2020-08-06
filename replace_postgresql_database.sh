0d7a621591e9260107a67a37d33a24313e6cb4d2
Thirumal <m.thirumal@hotmail.com>
2020-08-06 18:41:33 +0530

    close connection

--- replace_postgresql_database.sh
+++ replace_postgresql_database.sh
@@ -26,6 +26,7 @@ sudo -u postgres pg_dump $copyDatabase > $copyDatabaseFile
 echo "Database is dumped to " $copyDatabaseFile
 ## Delete the database
 queryToCloseConnection="select pg_terminate_backend(procpid) from pg_stat_activity where datname =$databaseToDelete" 
+echo "Closing connection " $queryToCloseConnection
 psql -U postgres -d $databaseToDelete -c $queryToCloseConnection
 
 sudo -u postgres dropdb $databaseToDelete;
