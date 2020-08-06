4bca0bebd5e08b527c83179e857592d16d6d9ea9
Thirumal <m.thirumal@hotmail.com>
2020-08-06 18:09:39 +0530

    Replace database

---
+++ replace_postgresql_database.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+################
+# Replace the database
+###Get the input from user
+sudo -u postgres
+echo "Enter the copy database name: "
+read copyDatabase
+currentDateTime=`date +"%Y-%m-%dT%T"`
+copyDatabaseFile="$copyDatabase_$currentDateTime.sql"
+echo "Database will be copied to " $copyDatabaseFile
+
+echo "Enter the database name to delete"
+read databaseToDelete;
+
+echo "Enter the new database name"
+read databaseToCreate;
+## Copy the database to file
+pg_dump $copyDatabase > $copyDatabaseFile
+echo "Database is dumped to " $copyDatabaseFile
+## Delete the database
+dropdb $databaseToDelete;
+echo $databaseToDelete " is deleted"
+## Create new dabase
+createdb $databaseToCreate;
+echo $databaseToCreate " is created"
+## Restore it to #$databaseToCreate
+psql -U postgres $databaseToCreate < $copyDatabaseFile
+##
+echo "Restore successfull@" `date +"%Y-%m-%dT%T"`
